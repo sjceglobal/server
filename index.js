104c5978ba2740078c494d9d3a18d49731619adc
jinseunglee <tjsqoo@gmail.com>
2024-10-06 16:57:31 +0900

    241005

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 import express from 'express';
 import { createClient } from 'redis';
+import { v4 as uuidv4 } from 'uuid';
+import pg from 'pg'
+const { Client } = pg
+
 import { createRequire } from 'module';
 
 // 추후 require가 필요한 경우 사용
@@ -7,6 +11,8 @@ const require = createRequire(import.meta.url);
 
 // Express 애플리케이션 생성
 const app = express();
+// 나중에 post방식으로 통신을 할 때 body값을 파싱하는데 사용할 예정
+app.use(express.json());
 const port = 80;
 
 // 기본 라우트 설정
@@ -17,8 +23,74 @@ app.get('/', async (req, res) => {
 });
 
 app.get('/login', async (req, res) => {
-    const value = await client.get(req.query.userid);
-    res.send(`사용자 정보 : ${value}`);
+    try {
+        try {
+            if (!req.query.userid) throw 'error';
+            if (!req.query.userpwd) throw 'error';
+        } catch (error) {
+            throw 'invalidate error';            
+        }
+        
+        let userInfo = {};
+        let sessionId;
+
+        try {
+            const res = await pgClient.query(`select * from users where userid = '${req.query.userid}';`);
+            console.log('user info:', res.rows[0]);
+            userInfo = res.rows[0];
+        } catch (error) {
+            console.error('오류 발생:', error);
+            throw 'database(postgres) error';
+        } 
+
+        try {
+            sessionId = uuidv4(); 
+            await client.set(sessionId, JSON.stringify(userInfo), 'EX', 1800);
+        } catch (error) {
+            console.error('Redis 저장 실패:', error);
+            throw 'database(redis) error';
+        } 
+
+        res.status(200).json({ sessionId });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: '서버 에러' });
+    }
+});
+
+app.get('/score', async (req, res) => {
+    try {
+        try {
+            if (!req.query.sessionid) throw 'error';
+        } catch (error) {
+            throw 'invalidate error';            
+        }
+        
+        let userInfo = {};
+        let scoreInfo = {};
+        const sessionId = req.query.sessionid;
+
+        try {
+            userInfo = JSON.parse(await client.get(sessionId));
+        } catch (error) {
+            console.error('Redis 로드 실패:', error);
+            throw 'database(redis) error';
+        } 
+
+        try {
+            const res = await pgClient.query(`select * from scores where userid = '${userInfo.userid}';`);
+            console.log('score info:', res.rows[0]);
+            scoreInfo = res.rows[0];
+        } catch (error) {
+            console.error('오류 발생:', error);
+            throw 'database(postgres) error';
+        } 
+
+        res.status(200).json(scoreInfo.score);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: '서버 에러' });
+    }
 });
 
 app.get('/test', async (req, res) => {
@@ -82,9 +154,20 @@ const client = await createClient()
   .on('error', err => console.log('Redis Client Error', err))
   .connect();
 
+const pgClient = new Client({
+    host: '127.0.0.1',       // 데이터베이스 호스트
+    port: 5432,              // PostgreSQL 포트 (기본값: 5432)
+    user: 'postgres',          // 데이터베이스 사용자
+    password: '123qwe1@qW', // 사용자 비밀번호
+    database: 'postgres',  // 데이터베이스 이름
+})
+await pgClient.connect()
+
 // 서버 종료 시 Redis 연결 종료
 process.on('SIGINT', async () => {
   await client.quit();
+  await pgClient.end();
+
   console.log('Redis 연결 종료.');
   process.exit(0);
 });
